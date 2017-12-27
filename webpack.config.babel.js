6ce58b8bb2762a6d6671bd91833dcdb29c2d0e48
Samuel Schumacher <samschu@live.com>
2017-12-27 16:22:13 -0600

    add Bulma to project

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,9 +1,22 @@
 var webpack = require('webpack');
 var path = require('path');
+const extractTextPlugin = require('extract-text-webpack-plugin');
 
-const OUTPUT_PATH = path.resolve('dist/js');
-const OUTPUT_FILENAME = '[name]-bundle.js';
+const SRC_PATH = path.join(__dirname, 'src');
+const DIST_PATH = path.join(__dirname, 'dst');
+
 const JS_ENTRY = path.resolve('src', 'js', 'index.js');
+const JS_OUTPUT_PATH = path.resolve('dist/js');
+const JS_OUTPUT_FILENAME = '[name]-bundle.js';
+
+const SASS_ENTRY = path.resolve('src', 'sass', 'main.scss');
+const SASS_OUTPUT_PATH = path.resolve('dist/css');
+const SASS_OUTPUT_FILENAME = '[name]-bundle.css';
+const SASS_LOADERS = [
+  'css-loader?minimize',
+  'sass-loader?indentedSyntax=sass&includePaths[]=' + SRC_PATH
+];
+
 const NODE_MODULES = path.resolve('node_modules');
 
 let esRule = {
@@ -18,13 +31,19 @@ let lintRule = {
   exclude: /(node_modules)/
 };
 
-const configuration = {
+let sassRule = {
+  test: /\.(sass|scss)$/,
+  use: extractTextPlugin.extract('css-loader!postcss-loader!sass-loader'),
+  exclude: /(node_modules)/
+};
+
+const jsConfiguration = {
   entry: {
     'js-library': JS_ENTRY
   },
   output: {
-    path: OUTPUT_PATH,
-    filename: OUTPUT_FILENAME
+    path: JS_OUTPUT_PATH,
+    filename: JS_OUTPUT_FILENAME
   },
   module: {
     rules: [esRule,lintRule]
@@ -34,4 +53,26 @@ const configuration = {
   }
 };
 
-module.exports = configuration;
+const sassConfiguration = {
+  entry: {
+    'style': SASS_ENTRY
+  },
+  output: {
+    path: SASS_OUTPUT_PATH,
+    filename: SASS_OUTPUT_FILENAME
+  },
+  module: {
+    rules: [sassRule]
+  },
+  plugins: [
+    new extractTextPlugin('[name].min.css')
+  ],
+  resolve: {
+    modules: [NODE_MODULES]
+  }
+};
+
+module.exports = [
+  jsConfiguration,
+  sassConfiguration
+];
